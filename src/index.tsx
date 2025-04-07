b315461b7b0c08996e24e281b50e9e6b606963fa
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2025-04-07 10:35:36 -0400

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -81,7 +81,7 @@
  arrFunctions: [async (...args) =>
         functions.setVar({ args, pass:{
           keyPath: ['example.A1.lists.iptsChanges.name'],
-          value: [teste de strg]
+          value: ["teste de strg"]
         }})]
  , trigger: 'on init'
 }})],            childrenItems:[() =><></>],
